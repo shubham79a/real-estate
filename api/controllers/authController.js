f5608bf6586fc9910a69be5fb31d4a3de61be9f9
shubham79a <shubh79.nolan@gmail.com>
2025-02-20 01:02:59 +0530

    prisma setup

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,15 +1,30 @@
 import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js";
 
-export const register = async(req, res) => {
+export const register = async (req, res) => {
     const { username, email, password } = req.body;
 
-    // hash
-    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
 
-    console.log(hashedPassword);
-    
+        // hash
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    // new user
+
+        // new user 
+        const newUser = await prisma.user.create({
+            data: {
+                username: username,
+                email: email,
+                password: hashedPassword,
+            },
+        })
+        console.log(newUser);
+    } catch (error) {
+        console.log(error.message);
+        
+    }
+
+
 
 
 }
